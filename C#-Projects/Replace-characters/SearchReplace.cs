3e065f495179f6970c23d785857366a8517c3a79
Michael Combrinck <64965281+SirknightMike@users.noreply.github.com>
2022-04-12 08:43:22 +0200

    Add files via upload

---
+++ C#-Projects/Replace-characters/SearchReplace.cs
@@ -0,0 +1,37 @@
+﻿namespace PRGYOURNAMEFA1
+{
+    public class SearchReplace
+    {
+        bool Set = false;
+        string UserString = "";
+
+        public string SearchSubstring(string UserString, string Find, string Replace)
+        {
+            if (!Set)
+            {
+                this.UserString = UserString;
+                Set = true;
+            }
+
+            if (this.UserString.Contains(Find))
+                ReplaceSubstring(Find, Replace);
+
+            while (this.UserString.Contains(Find))
+                SearchSubstring(this.UserString, Find, Replace);
+
+            return this.UserString;
+        }
+
+        public void ReplaceSubstring(string Find, string Replace)
+        {
+            string NewUserString = UserString;
+
+            int Index = NewUserString.IndexOf(Find);
+            int Length = Find.Length;
+
+            NewUserString = NewUserString.Substring(0, Index) + Replace + NewUserString.Substring(Index + Length);
+
+            UserString = NewUserString;
+        }
+    }
+}
